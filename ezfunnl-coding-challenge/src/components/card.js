6b3d7dd88b3cf2306e0638716bc82f11ef93f8f5
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-19 12:41:12 -0500

    add state to Card and checkbox for each displayd restaurant

--- ezfunnl-coding-challenge/src/components/card.js
+++ ezfunnl-coding-challenge/src/components/card.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 
+
 export default class Card extends React.Component {
+constructor(props) {
+    super(props)
+    this.state = {
+        restaurants: []
+    }
+}
+
     render(){
         return(
+        <div>
             <div className="restaurant_name">
-                {this.props.restaurant.name}
+                {/* {this.props.restaurant.name} */}
+                <form>
+                    {this.props.restaurant.name}{' : '}
+                    {/* Visited? */}
+                    <input type="checkbox" value={this.props.restaurant.name} id="checkbox" ></input>
+                </form>
             </div>
+        </div>
         )
     }
 }
+//onChange={localStorage.setItem("list", JSON.stringify(this.props.restaurant.name))}
