e4c3fc4c1efcee7bffafdebd363967c7cd65a371
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 15:34:21 -0500

    refactor card component to functional component

--- ezfunnl-coding-challenge/src/components/card.js
+++ ezfunnl-coding-challenge/src/components/card.js
@@ -2,28 +2,48 @@ import React from 'react'
 
 
 
-export default class Card extends React.Component {
+// export default class Card extends React.Component {
 
-    render(){
-        return(
+//     render(){
+//         return(
+//         <div>
+//             <div className="restaurant_name">
+//                 {/* {this.props.restaurant.name} */}
+//                 <form>
+//                     {this.props.restaurant.name}{' : '}
+//                     {/* Visited? */}
+//                     <input
+//                         type="checkbox"
+//                         value={this.props.restaurant.name}
+//                         id="checkbox"
+//                         checked={this.props.isVisited}
+//                         data-restaurant-id={this.props.restaurant.id}
+//                         onChange={this.props.onVisited}
+//                     />
+//                 </form>
+//             </div>
+//         </div>
+//         )
+//     }
+// }
+//onChange={localStorage.setItem("list", JSON.stringify(this.props.restaurant.name))}
+
+export default function Card(props){
+    return(
         <div>
             <div className="restaurant_name">
-                {/* {this.props.restaurant.name} */}
                 <form>
-                    {this.props.restaurant.name}{' : '}
-                    {/* Visited? */}
+                    {props.restaurant.name}{' : '}
                     <input
                         type="checkbox"
-                        value={this.props.restaurant.name}
+                        value={props.restaurant.name}
                         id="checkbox"
-                        checked={this.props.isVisited}
-                        data-restaurant-id={this.props.restaurant.id}
-                        onChange={this.props.onVisited}
+                        checked={props.isVisited}
+                        data-restaurant-id={props.restaurant.id}
+                        onChange={props.onVisited}
                     />
                 </form>
             </div>
         </div>
         )
-    }
 }
-//onChange={localStorage.setItem("list", JSON.stringify(this.props.restaurant.name))}
