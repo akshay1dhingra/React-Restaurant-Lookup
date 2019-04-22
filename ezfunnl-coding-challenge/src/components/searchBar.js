f4ac38f027696a409e1dfe91b3e1ce8f339f3f4d
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 16:41:56 -0500

    rf SearchBar to functional component

--- ezfunnl-coding-challenge/src/components/searchBar.js
+++ ezfunnl-coding-challenge/src/components/searchBar.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
-export default class SearchBar extends React.Component {
-    render(){
-        return(
-            <div className="search_bar">
-                <form onSubmit={this.props.onSubmitForm}>
-                    <label>
-                        Zipcode: 
-                        <input type="text" name="search" value={this.props.search} onChange={this.props.onSearchChange}/>
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        )
-    }
+export default function SearchBar(props){
+    return( 
+        <div className="search_bar">
+            <form onSubmit={props.onSubmitForm}>
+                <label>
+                    Zipcode: 
+                    <input type="text" name="search" value={props.search} onChange={props.onSearchChange}/>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
 }
