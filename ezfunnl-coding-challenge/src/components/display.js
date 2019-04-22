51c0c7ff903932f85092ad42b48a438a4a7a4ed1
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 15:35:49 -0500

    refactor display component to functional component

--- ezfunnl-coding-challenge/src/components/display.js
+++ ezfunnl-coding-challenge/src/components/display.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import Card from './card'
 
-export default class Display extends React.Component {
-    render(){
-        let cardComponent = this.props.searchResults.map(r => (
-            <Card 
-                restaurant={r}
-                key={r.id}
-                onVisited={this.props.onVisited}
-                isVisited={this.props.isVisitedFunc(r.id)}
-            />
-          ))
-
-
-        return(
-            <div className="display_results">
-                {cardComponent}
-            </div>
-        )
-    }
+export default function Display(props){
+    let cardComponent = props.searchResults.map(r => (
+        <Card 
+            restaurant={r}
+            key={r.id}
+            onVisited={props.onVisited}
+            isVisited={props.isVisitedFunc(r.id)}
+        />
+      ))
+      
+    return(
+        <div className="display_results">
+            {cardComponent}
+        </div>
+    )
 }
