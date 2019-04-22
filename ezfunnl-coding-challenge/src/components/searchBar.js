3e4ec5c8eb483331e692efdb3dbd261e8d8d410a
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 14:59:50 -0500

    rf SearchBar with props

--- ezfunnl-coding-challenge/src/components/searchBar.js
+++ ezfunnl-coding-challenge/src/components/searchBar.js
@@ -4,10 +4,10 @@ export default class SearchBar extends React.Component {
     render(){
         return(
             <div className="search_bar">
-                <form onSubmit={this.props.searchBar}>
+                <form onSubmit={this.props.onSubmitForm}>
                     <label>
                         Zipcode: 
-                        <input type="text" />
+                        <input type="text" name="search" value={this.props.search} onChange={this.props.onSearchChange}/>
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
