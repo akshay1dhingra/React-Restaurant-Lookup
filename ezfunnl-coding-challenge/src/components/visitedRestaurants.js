d0de541b5668cf67d30ee3fea1218a67cf797aa5
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 15:42:12 -0500

    refactor VisitedRestaurant component to function

--- ezfunnl-coding-challenge/src/components/visitedRestaurants.js
+++ ezfunnl-coding-challenge/src/components/visitedRestaurants.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
-export default class VisitedRestaurants extends React.Component {
+// export default class VisitedRestaurants extends React.Component {
 
     
 
-    render(){
-        return (
-            <div className="local_storage_data">
-                {this.props.restaurants.map(r => <div>{r.name}</div>)}
-            </div>
-        );
-    }
+//     render(){
+//         return (
+//             <div className="local_storage_data">
+//                 {this.props.restaurants.map(r => <div key={r.id}>{r.name}</div>)}
+//             </div>
+//         );
+//     }
+// }
+
+export default function VisitedRestaurants(props){
+    return (
+        <div className="local_storage_data">
+            {props.restaurants.map(r => <div key={r.id}>{r.name}</div>)}
+        </div>
+    );
 }
