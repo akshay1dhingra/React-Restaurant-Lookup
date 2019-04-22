f659d14fe52be5d1687dbc92c91b205569328208
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-04-22 14:47:23 -0500

    rf Display to render checkmark(yes) if object exists in localStorage

--- ezfunnl-coding-challenge/src/components/display.js
+++ ezfunnl-coding-challenge/src/components/display.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Card from './card'
 
-export default class Dispaly extends React.Component {
+export default class Display extends React.Component {
     render(){
         let cardComponent = this.props.searchResults.map(r => (
             <Card 
                 restaurant={r}
                 key={r.id}
                 onVisited={this.props.onVisited}
+                isVisited={this.props.isVisitedFunc(r.id)}
             />
           ))
 
